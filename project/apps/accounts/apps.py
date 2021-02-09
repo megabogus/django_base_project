16b8a91051e0bea1bc4e3e8511fe55ca40df9cf3
Alexander Zolotarev <megabogus84@gmail.com>
2021-02-09 16:23:42 +0300

    fix gitlab

--- project/apps/accounts/apps.py
+++ project/apps/accounts/apps.py
@@ -4,8 +4,6 @@ from django.db.models.signals import post_save
 from django.utils.translation import gettext_lazy as _
 from fieldsignals import post_save_changed
 
-User = get_user_model()
-
 
 class AccountsConfig(AppConfig):
     name = 'apps.accounts'
@@ -13,6 +11,8 @@ class AccountsConfig(AppConfig):
 
     def ready(self):
         from .signals import change_staff_perms, set_default_perms_to_user, set_perms_to_staff
+        User = get_user_model()
+
         post_save_changed.connect(change_staff_perms, sender=User, fields=('is_staff',))
         post_save.connect(set_default_perms_to_user, sender=User)
         post_save.connect(set_perms_to_staff, sender=User)
